448e3c2845fd7e798a9e59a46367d1e6bbfd0e46
Ben Croker <ben@putyourlightson.net>
2020-09-19 00:15:48 +0200

    Added loading spinner

--- src/resources/js/playground.js
+++ src/resources/js/playground.js
@@ -1,9 +1,11 @@
 htmx.on('htmx:beforeRequest', function(event) {
+    $('.spinner').show();
     event.detail.xhr.setRequestHeader('Sprig-Playground-Component', encodeURIComponent($('#component').val()));
     event.detail.xhr.setRequestHeader('Sprig-Playground-Variables', $('#variables').val());
 });
 
 htmx.on('htmx:afterSwap', function(event) {
+    $('.spinner').hide();
     $('#sourcecode').val('');
 
     var sourcecode = $('#playground').html();
@@ -14,6 +16,8 @@ htmx.on('htmx:afterSwap', function(event) {
 });
 
 $('#run').click(function() {
+    $('.playground .btn.submit').removeClass('submit');
+
     document.getElementById('playground').dispatchEvent(new Event('refresh'));
 });
 
