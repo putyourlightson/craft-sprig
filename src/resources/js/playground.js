85b976a0e842f3e2f2a5fb8fbe2c748e23e21a46
Ben Croker <ben@putyourlightson.net>
2020-09-19 15:33:37 +0200

    Styling and request headers

--- src/resources/js/playground.js
+++ src/resources/js/playground.js
@@ -1,8 +1,20 @@
-htmx.on('htmx:beforeRequest', function(event) {
+var sprigInclude = true;
+
+htmx.on('htmx:configRequest', function(event) {
     $('.spinner').show();
-    event.detail.xhr.setRequestHeader('Sprig-Playground-Component', encodeURIComponent($('#component').val()));
-    event.detail.xhr.setRequestHeader('Sprig-Playground-Variables', $('#variables').val());
+
+    event.detail.headers['Sprig-Playground-Component'] = encodeURIComponent($('#component').val());
+    event.detail.headers['Sprig-Playground-Variables'] = $('#variables').val();
+
+    if (sprigInclude) {
+        event.detail.headers['HX-Request'] = 'false';
+        sprigInclude = false;
+    }
+
+    console.log(event.detail.headers);
 });
+
+
 
 htmx.on('htmx:afterSwap', function(event) {
     $('.spinner').hide();
@@ -17,6 +29,8 @@ htmx.on('htmx:afterSwap', function(event) {
 
 $('#run').click(function() {
     $('.playground .btn.submit').removeClass('submit');
+
+    sprigInclude = true;
 
     document.getElementById('playground').dispatchEvent(new Event('refresh'));
 });
