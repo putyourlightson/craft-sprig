508c614aa80c1fe94468203e3c51b6bfc50ba55d
Ben Croker <ben@putyourlightson.com>
2021-10-04 19:12:03 +0200

    Added ComponentsController back in

---
+++ src/controllers/ComponentsController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @copyright Copyright (c) PutYourLightsOn
+ */
+
+namespace putyourlightson\sprig\plugin\controllers;
+
+/**
+ * This class exists for backwards compatibility only.
+ * TODO: remove in Sprig 2.0.0
+ *
+ * @since 1.9.0
+ */
+class ComponentsController extends \putyourlightson\sprig\controllers\ComponentsController
+{
+}
