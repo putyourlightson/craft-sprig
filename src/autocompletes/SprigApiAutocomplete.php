f20d1905d8cc5e12c156588ec244667673624ea8
Andrew Welch <andrew@nystudio107.com>
2022-06-10 01:13:30 -0400

    feat: Use Twigfield

---
+++ src/autocompletes/SprigApiAutocomplete.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @copyright Copyright (c) PutYourLightsOn
+ */
+
+namespace putyourlightson\sprig\plugin\autocompletes;
+
+use nystudio107\twigfield\base\Autocomplete;
+use nystudio107\twigfield\models\CompleteItem;
+use nystudio107\twigfield\types\AutocompleteTypes;
+use nystudio107\twigfield\types\CompleteItemKind;
+
+class SprigApiAutocomplete extends Autocomplete
+{
+    // Public Static Methods
+    // =========================================================================
+
+    /**
+     * @inerhitDoc
+     */
+    public static function getAutocompleteName(): string
+    {
+        return 'SprigApiAutocomplete';
+    }
+
+    /**
+     * @inerhitDoc
+     */
+    public static function getAutocompleteType(): string
+    {
+        return AutocompleteTypes::TwigExpressionAutocomplete;
+    }
+
+    /**
+     * Core function that generates the autocomplete array
+     */
+    public static function generateCompleteItems(): void
+    {
+        CompleteItem::create()
+            ->label('sprig')
+            ->insertText('sprig')
+            ->kind(CompleteItemKind::FunctionKind)
+            ->sortText('__sprig')
+            ->add(self::class);
+    }
+}
--- src/resources/js/playground.js
+++ src/resources/js/playground.js
@@ -1,160 +1,68 @@
-$(document).ready(function()
-{
-    //--- Monaco editor ---//
+$(document).ready(function () {
+  //--- htmx ---//
 
-    let editor;
+  htmx.on('htmx:configRequest', function (event) {
+    $('.spinner').show();
 
-    require.config({ paths: { 'vs': resourcesUrl + '/lib/monaco-editor/min/vs' }});
-    window.MonacoEnvironment = { getWorkerUrl: () => proxy };
+    event.detail.headers['Sprig-Playground-Component'] = editor.getValue();
+    event.detail.headers['Sprig-Playground-Variables'] = $('#input-variables').val();
 
-    let proxy = URL.createObjectURL(new Blob([`
-        self.MonacoEnvironment = {
-            baseUrl: '` + resourcesUrl + `/lib/monaco-editor/min/'
-        };
-        importScripts('` + resourcesUrl + `/lib/monaco-editor/min/vs/base/worker/workerMain.js');
-    `], { type: 'text/javascript' }));
+    if ($('#playground').html() == '') {
+      event.detail.headers['HX-Request'] = 'false';
+    }
+  });
 
-    require(["vs/editor/editor.main"], function () {
-        monaco.languages.registerCompletionItemProvider('twig', {
-            provideCompletionItems: getCompletionItems,
-        });
+  htmx.on('htmx:afterSwap', function (event) {
+    $('.spinner').hide();
+    $('#sourcecode').val(html_beautify(event.detail.xhr.responseText));
 
-        import(resourcesUrl + "/js/autocomplete.js");
+    $('#output-variables').val(event.detail.xhr.getResponseHeader('Sprig-Playground-Variables'));
+  });
 
-        editor = monaco.editor.create($('#editor')[0], {
-            language: 'twig',
-            value: $('#input').val(),
-            wordWrap: true,
-            scrollBeyondLastLine: false,
-            lineNumbersMinChars: 4,
-            fontSize: 14,
-            fontFamily: 'SFMono-Regular, Consolas, "Liberation Mono", Menlo, Courier, monospace',
-            minimap: {
-                enabled: false
-            },
-        });
-    });
-
-
-    //--- htmx ---//
-
-    htmx.on('htmx:configRequest', function(event) {
-        $('.spinner').show();
-
-        event.detail.headers['Sprig-Playground-Component'] = editor.getValue();
-        event.detail.headers['Sprig-Playground-Variables'] = $('#input-variables').val();
-
-        if ($('#playground').html() == '') {
-            event.detail.headers['HX-Request'] = 'false';
-        }
-    });
-
-    htmx.on('htmx:afterSwap', function(event) {
-        $('.spinner').hide();
-        $('#sourcecode').val(html_beautify(event.detail.xhr.responseText));
-
-        $('#output-variables').val(event.detail.xhr.getResponseHeader('Sprig-Playground-Variables'));
-    });
-
-    htmx.on('htmx:responseError', function(event) {
-        $('.spinner').hide();
-        $('#playground').html(`
+  htmx.on('htmx:responseError', function (event) {
+    $('.spinner').hide();
+    $('#playground').html(`
         <h2 class="error">` + event.detail.xhr.status + ' ' + event.detail.xhr.statusText + `</h2>
         <p>View the full response in the network tab of your browser dev tools.</p>
     `);
-        $('#sourcecode').val('');
-    });
+    $('#sourcecode').val('');
+  });
 
-    //--- Buttons ---//
+  //--- Buttons ---//
 
-    $('#create').click(function(event) {
-        event.preventDefault();
+  $('#create').click(function (event) {
+    event.preventDefault();
 
-        $('#playground').html('');
-        $('#sourcecode').val('');
-        $('#output-variables').val('');
+    $('#playground').html('');
+    $('#sourcecode').val('');
+    $('#output-variables').val('');
 
-        $('#playground')[0].dispatchEvent(new Event('refresh'));
-    });
+    $('#playground')[0].dispatchEvent(new Event('refresh'));
+  });
 
-    $('#output-toggle').click(function(event) {
-        event.preventDefault();
-        $(this).toggleClass('submit');
+  $('#output-toggle').click(function (event) {
+    event.preventDefault();
+    $(this).toggleClass('submit');
 
-        $('.playground #playground').toggle();
-        $('.playground #sourcecode').toggle();
-    });
+    $('.playground #playground').toggle();
+    $('.playground #sourcecode').toggle();
+  });
 
-    $('#action-form').submit(function(event) {
-        $(this).find('textarea[name=component]').val(editor.getValue());
-        $(this).find('textarea[name=variables]').val($('#input-variables').val());
+  $('#action-form').submit(function (event) {
+    $(this).find('textarea[name=component]').val(editor.getValue());
+    $(this).find('textarea[name=variables]').val($('#input-variables').val());
 
-        if ($(this).find('input[name=action]:last').val() == 'sprig/playground/save') {
-            let name = prompt('Enter a name for this component.');
+    if ($(this).find('input[name=action]:last').val() == 'sprig/playground/save') {
+      let name = prompt('Enter a name for this component.');
 
-            if (name) {
-                $(this).find('input[name=name]').val(name);
-                return;
-            }
-        }
-        else {
-            return;
-        }
-
-        event.preventDefault();
-    });
-});
-
-
-function getCompletionItems()
-{
-    let suggestions = [
-        {
-            label: 'sprig',
-            insertText: 'sprig',
-            kind: monaco.languages.CompletionItemKind.Function,
-            sortText: '_sprig',
-        }
-    ];
-
-    let suggestionLabels = [
-        's-action=""',
-        's-listen=""',
-        's-method=""', 's-method="post"',
-        's-boost=""', 's-boost="true"',
-        's-confirm=""', 's-confirm="Are you sure?"',
-        's-disable=""',
-        's-disinherit=""', 's-disinherit="*"',
-        's-encoding=""', 's-encoding="multipart/form-data"',
-        's-ext=""',
-        's-headers=""',
-        's-history-elt=""',
-        's-include=""',
-        's-indicator=""',
-        's-params=""',
-        's-preserve=""', 's-preserve="true"',
-        's-prompt=""',
-        's-push-url=""',
-        's-replace=""',
-        's-request=""',
-        's-select=""',
-        's-swap=""', 's-swap="innerHTML"', 's-swap="outerHTML"', 's-swap="beforebegin"', 's-swap="afterbegin"', 's-swap="beforeend"', 's-swap="afterend"',
-        's-swap-oob=""', 's-swap-oob="true"',
-        's-target=""', 's-target="this"',
-        's-trigger=""', 's-trigger="click"', 's-trigger="change"', 's-trigger="submit"',
-        's-val:x="1"', 's-val:y="2"',
-        's-vals="{x:1,y:2}"',
-    ];
-
-    for (let i = 0; i < suggestionLabels.length; i++) {
-        suggestions[i + 1] = {
-            label: suggestionLabels[i],
-            insertText: suggestionLabels[i],
-            kind: monaco.languages.CompletionItemKind.Field,
-        };
+      if (name) {
+        $(this).find('input[name=name]').val(name);
+        return;
+      }
+    } else {
+      return;
     }
 
-    return {
-        suggestions: suggestions,
-    };
-}
+    event.preventDefault();
+  });
+});
