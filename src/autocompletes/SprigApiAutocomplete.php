dc269dd5d7bb900ad35fafa2d82257d1525b5499
Ben Croker <ben@putyourlightson.com>
2022-06-11 17:45:18 +0200

    Add attributes, docs and descriptions

--- src/autocompletes/SprigApiAutocomplete.php
+++ src/autocompletes/SprigApiAutocomplete.php
@@ -5,6 +5,7 @@
 
 namespace putyourlightson\sprig\plugin\autocompletes;
 
+use Craft;
 use nystudio107\twigfield\base\Autocomplete;
 use nystudio107\twigfield\models\CompleteItem;
 use nystudio107\twigfield\types\AutocompleteTypes;
@@ -13,28 +14,187 @@ use nystudio107\twigfield\types\CompleteItemKind;
 class SprigApiAutocomplete extends Autocomplete
 {
     public const SPRIG_ATTRIBUTES = [
-        's-action=""',
-        's-method=""', 's-method="post"',
-        's-boost=""', 's-boost="true"',
-        's-confirm=""', 's-confirm="Are you sure?"',
-        's-disable',
-        's-encoding=""', 's-encoding="multipart/form-data"',
-        's-headers=""',
-        's-history-elt=""',
-        's-include=""',
-        's-indicator=""',
-        's-params=""',
-        's-preserve=""', 's-preserve="true"',
-        's-prompt=""',
-        's-push-url=""',
-        's-request=""',
-        's-select=""',
-        's-swap=""', 's-swap="innerHTML"', 's-swap="outerHTML"', 's-swap="beforebegin"', 's-swap="afterbegin"', 's-swap="beforeend"', 's-swap="afterend"',
-        's-swap-oob=""',
-        's-target=""', 's-target="this"',
-        's-trigger=""', 's-trigger="click"', 's-trigger="change"', 's-trigger="submit"',
-        's-val-x="1"', 's-val-y="2"',
+        's-action' => [
+            'values' => [
+                's-action=""',
+            ],
+            'description' => 'Sends an action request to the provided controller action.',
+        ],
+        's-boost' => [
+            'values' => [
+                's-boost=""',
+                's-boost="true"',
+            ],
+            'description' => 'Boosts normal anchors and form tags to use AJAX instead.',
+        ],
+        's-confirm' => [
+            'values' => [
+                's-confirm=""',
+                's-confirm="Are you sure?"',
+            ],
+            'description' => 'Shows a `confim()` dialog before issuing a request.',
+        ],
+        's-disable' => [
+            'values' => [
+                's-disable',
+            ],
+            'description' => 'Disables htmx processing for a given element and its children.',
+        ],
+        's-disinherit' => [
+            'values' => [
+                's-disinherit=""',
+                's-disinherit="*"',
+            ],
+            'description' => 'Allows you to control attribute inheritance.',
+        ],
+        's-encoding' => [
+            'values' => [
+                's-encoding=""',
+                's-encoding="multipart/form-data"',
+            ],
+            'description' => 'Allows you to change the request encoding from the usual `application/x-www-form-urlencoded` to `multipart/form-data`, useful for when you want to support file uploads.',
+        ],
+        's-ext' => [
+            'values' => [
+                's-ext=""',
+            ],
+            'description' => 'Enables an htmx extension for an element and all its children.',
+        ],
+        's-headers' => [
+            'values' => [
+                's-headers=""',
+            ],
+            'description' => 'Allows you to add to the headers that will be submitted with an AJAX request.',
+        ],
+        's-history-elt' => [
+            'values' => [
+                's-history-elt=""',
+            ],
+            'description' => 'Allows you to specify the element that will be used to snapshot and restore page state during navigation.',
+        ],
+        's-include' => [
+            'values' => [
+                's-include=""',
+            ],
+            'description' => 'Includes additional element values in AJAX requests.',
+        ],
+        's-indicator' => [
+            'values' => [
+                's-indicator=""',
+            ],
+            'description' => 'The element to put the `htmx-request` class on during the AJAX request.',
+        ],
+        's-listen' => [
+            'values' => [
+                's-listen=""',
+            ],
+            'description' => 'Allows you to specify one or more components (as CSS selectors, separated by commas) that when refreshed, should trigger a refresh on the current element.',
+        ],
+        's-method' => [
+            'values' => [
+                's-method=""',
+                's-method="post"',
+            ],
+            'description' => 'Forces the request to be of the type provided. Possible values are `get` (default) or `post`. If set to `post`, Sprig automatically sends a CSRF token in the request.',
+        ],
+        's-params' => [
+            'values' => [
+                's-params=""',
+            ],
+            'description' => 'Filters the parameters that will be submitted with a request.',
+        ],
+        's-preserve' => [
+            'values' => [
+                's-preserve="true"',
+            ],
+            'description' => 'Ensures that an element remains unchanged even when the component is re-rendered. The value should be set to `true` and the element must have an `id`.',
+        ],
+        's-prompt' => [
+            'values' => [
+                's-prompt=""',
+            ],
+            'description' => 'Shows a prompt before submitting a request.',
+        ],
+        's-push-url' => [
+            'values' => [
+                's-push-url=""',
+            ],
+            'description' => 'Pushes a URL into the URL bar and creates a new history entry.',
+        ],
+        's-replace' => [
+            'values' => [
+                's-replace=""',
+            ],
+            'description' => 'Specifies the element to be replaced. The entire component is returned in the response, but only the specified element is replaced. This is equivalent to combining `s-select`, `s-target` and `s-swap`.',
+        ],
+        's-request' => [
+            'values' => [
+                's-request=""',
+            ],
+            'description' => 'Allows you to configure various aspects of the request.',
+        ],
+        's-select' => [
+            'values' => [
+                's-select=""',
+            ],
+            'description' => 'Selects a subset of the server response to process.',
+        ],
+        's-swap' => [
+            'values' => [
+                's-swap=""',
+                's-swap="innerHTML"',
+                's-swap="outerHTML"',
+                's-swap="beforebegin"',
+                's-swap="afterbegin"',
+                's-swap="beforeend"',
+                's-swap="afterend"',
+            ],
+            'description' => 'Controls how the response content is swapped into the DOM (`outerHTML`, `beforeEnd`, etc.).',
+        ],
+        's-swap-oob' => [
+            'values' => [
+                's-swap-oob=""',
+            ],
+            'description' => 'Marks content in a response as being “Out of Band”, i.e. swapped somewhere other than the target.',
+        ],
+        's-sync' => [
+            'values' => [
+                's-sync=""',
+            ],
+            'description' => 'Allows you to synchronize AJAX requests between multiple elements.',
+        ],
+        's-target' => [
+            'values' => [
+                's-target=""',
+                's-target="this"',
+            ],
+            'description' => 'Specifies the target element to be swapped.',
+        ],
+        's-trigger' => [
+            'values' => [
+                's-trigger=""',
+                's-trigger="click"',
+                's-trigger="change"',
+                's-trigger="submit"',
+            ],
+            'description' => 'Specifies the event that triggers the request.',
+        ],
+        's-val' => [
+            'values' => [
+                's-val-x="1"',
+                's-val-y="2"',
+            ],
+            'description' => 'Provides a more readable way of populating the `s-vals` attribute.',
+        ],
+        's-vals' => [
+            'values' => [
+                's-vals=""',
+            ],
+            'description' => 'Adds to the parameters that will be submitted with the request. The value must be a JSON encoded list of name-value pairs.',
+        ],
     ];
+
+    public const DOCS_URL = 'https://putyourlightson.com/plugins/sprig#';
 
     /**
      * @inheritdoc
@@ -44,26 +204,36 @@ class SprigApiAutocomplete extends Autocomplete
     /**
      * @inheritdoc
      */
-    public $type = AutocompleteTypes::TwigExpressionAutocomplete;
+    public $type = AutocompleteTypes::GeneralAutocomplete;
 
     /**
      * @inheritdoc
      */
     public function generateCompleteItems(): void
     {
+        $detail = Craft::t('sprig', 'Sprig Attribute');
+
         CompleteItem::create()
             ->label('sprig')
             ->insertText('sprig')
-            ->kind(CompleteItemKind::FunctionKind)
-            ->sortText('__sprig')
+            ->detail($detail)
+            ->documentation(Craft::t('sprig', 'Adding the sprig attribute to elements makes them trigger a component re-render.'))
+            ->kind(CompleteItemKind::FieldKind)
+            ->sortText('_sprig')
             ->add($this);
 
-        foreach (self::SPRIG_ATTRIBUTES as $attribute) {
-            CompleteItem::create()
-                ->label($attribute)
-                ->insertText($attribute)
-                ->kind(CompleteItemKind::FieldKind)
-                ->add($this);
+        foreach (self::SPRIG_ATTRIBUTES as $name => $attribute) {
+            $docs = '[' . $name . '](' . self::DOCS_URL . $name . ') | ' . Craft::t('sprig', $attribute['description']);
+
+            foreach ($attribute['values'] as $value) {
+                CompleteItem::create()
+                    ->label($value)
+                    ->insertText($value)
+                    ->detail($detail)
+                    ->documentation($docs)
+                    ->kind(CompleteItemKind::FieldKind)
+                    ->add($this);
+            }
         }
     }
 }
