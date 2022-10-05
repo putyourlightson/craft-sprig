4dfd74dfbf9c9454d1fe5ab448b1d2389410a74d
Ben Croker <ben@putyourlightson.com>
2022-10-05 11:39:24 +0200

    ECS

--- src/Sprig.php
+++ src/Sprig.php
@@ -82,7 +82,7 @@ class Sprig extends Plugin
     private function _registerCpRoutes()
     {
         Event::on(UrlManager::class, UrlManager::EVENT_REGISTER_CP_URL_RULES,
-            function (RegisterUrlRulesEvent $event) {
+            function(RegisterUrlRulesEvent $event) {
                 $event->rules['sprig'] = 'sprig/playground/index';
                 $event->rules['sprig/<id:\d+>'] = 'sprig/playground/index';
                 $event->rules['sprig/<slug:([^\/]*)?>'] = 'sprig/playground/index';
@@ -96,7 +96,7 @@ class Sprig extends Plugin
     private function _registerAutocompletes()
     {
         Event::on(AutocompleteService::class, AutocompleteService::EVENT_REGISTER_TWIGFIELD_AUTOCOMPLETES,
-            function (RegisterTwigfieldAutocompletesEvent $event) {
+            function(RegisterTwigfieldAutocompletesEvent $event) {
                 if ($event->fieldType === self::SPRIG_TWIG_FIELD_TYPE) {
                     $event->types[] = SprigApiAutocomplete::class;
                 }
